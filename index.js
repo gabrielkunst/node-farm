59b1f994912105b87e4394da2e55572bbd504a01
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-09-16 12:21:51 -0300

    feat - added prettier

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
-const replaceTemplate = require('./modules/replaceTemplate')
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const templateOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  'utf-8'
-)
+  "utf-8"
+);
 const templateProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  'utf-8'
-)
+  "utf-8"
+);
 const templateCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  'utf-8'
-)
+  "utf-8"
+);
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 // createServer receives a callback function that will run every on every new request on the server
 const server = http.createServer((req, res) => {
-  const { pathname, query } = url.parse(req.url, true)
+  const { pathname, query } = url.parse(req.url, true);
 
-  if (pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, { 'Content-type': 'text/html' })
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
 
     const htmlCards = dataObj
       .map(card => replaceTemplate(templateCard, card))
-      .join('')
+      .join("");
     const overviewPage = templateOverview.replace(
-      '{%PRODUCT_CARDS%}',
+      "{%PRODUCT_CARDS%}",
       htmlCards
-    )
+    );
 
-    res.end(overviewPage)
-  } else if (pathname === '/product') {
-    res.writeHead(200, { 'Content-type': 'text/html' })
-    const product = dataObj[query?.id]
-    const productPage = replaceTemplate(templateProduct, product)
-    res.end(productPage)
-  } else if (pathname === '/api') {
+    res.end(overviewPage);
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query?.id];
+    const productPage = replaceTemplate(templateProduct, product);
+    res.end(productPage);
+  } else if (pathname === "/api") {
     res.writeHead(200, {
-      'Content-type': 'application/json'
-    })
-    res.end(data)
+      "Content-type": "application/json"
+    });
+    res.end(data);
   } else {
-    res.end('page not found')
+    res.end("page not found");
   }
-})
+});
 
 // adding the server port and a callback that will run on the first load
 server.listen(8000, () => {
-  console.log('Server listening on localhost:8000')
-})
+  console.log("Server listening on localhost:8000");
+});
